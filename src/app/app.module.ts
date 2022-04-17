0f196a8eb1cdb55010ab22458138ec84f7b56f64
kiansahafi <kiansahafi1@gmail.com>
2022-04-17 10:21:49 +0430

    [feat] added the formControl and formGroup to the student component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,6 +29,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatMenuModule,
     HttpClientModule,
+    MatFormFieldModule,
     MatIconModule,
   ],
   providers: [AppConfigService],
--- src/app/pages/components/student-page/student-edit/student-edit.component.ts
+++ src/app/pages/components/student-page/student-edit/student-edit.component.ts
@@ -1,11 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-edit',
   templateUrl: './student-edit.component.html',
 })
 export class StudentEditComponent implements OnInit {
-  constructor() {}
+  private isOnEditMode: boolean = false;
+  private studentId: string = '';
+  email = new FormControl('');
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
-  ngOnInit(): void {}
+  studentForm = this.formbuilder.group({
+    FirstName: '',
+    LastName: '',
+    EmailAddress: ['', Validators.email],
+    Country: '',
+    StreetAddress: '',
+    City: '',
+    Region: '',
+    PostalCode: '',
+  });
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.url.subscribe(this.onRouteChange.bind(this));
+  }
+
+  onRouteChange(res: any) {
+    if (res[1].path == 'edit') {
+      this.studentId = res[2].path;
+      this.getData();
+    } else if (res[1].path == 'Insert') {
+      this.isOnEditMode = false;
+    }
+  }
+
+  getData() {
+    //get the data from backend and fill the forms for edit
+  }
+  onSubmit() {
+    if (this.isOnEditMode == true) {
+      this.editData();
+    } else {
+      this.insertData();
+    }
+  }
+
+  editData() {
+    //calling the backend to edit the data
+  }
+
+  insertData() {
+    //calling the backend to insert the new data
+  }
 }
